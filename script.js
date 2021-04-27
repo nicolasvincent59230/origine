2fbab1915b22625b5b041edb610efc883f8ac9b4
nicolasvincent59230 <nicolasvincent59230@gmail.com>
2021-04-27 10:58:52 +0200

    correction

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(function(){
+
+
     $picker = $( "#datePicker" ).datepicker({
         monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
         monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
@@ -8,9 +10,13 @@ $(function(){
         weekHeader: 'Semaines',
         dateFormat: 'dd-mm-yy',
         showWeek: true,
-        minDate: 0,
-        onSelect: function (date) {
-            $(this).datepicker('setDate', "+9w");
-        }
+        minDate: 0
+    })
+
+    $('#datePicker').change(function() {
+        var date2 = $('#datePicker').datepicker('getDate', '+9w');
+        date2.setDate(date2.getDate()+70);
+        $('#datePicker').datepicker('setDate', date2);
     });
+
 });
